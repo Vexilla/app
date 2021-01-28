37cac377148ff7769418a8d4a4e853f4e6d5e2a6
Chris Griffing <cmgriffing@gmail.com>
2021-01-28 11:58:36 -0800

    fix: use proper formatting for package.json title and set title via vue cli config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
+  pages: {
+    index: {
+      entry: 'src/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Vexilla Config UI',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  }
 };
