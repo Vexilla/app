93a4a892af0faa1f01a08f6aef467263bb0aa657
Chris Griffing <cmgriffing@gmail.com>
2021-01-28 12:40:57 -0800

    fix: update entry point for vue cli

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
   pages: {
     index: {
-      entry: 'src/index/main.js',
-      template: 'public/index.html',
+      entry: './src/main.ts',
+      template: './public/index.html',
       filename: 'index.html',
       title: 'Vexilla Config UI',
       chunks: ['chunk-vendors', 'chunk-common', 'index']
