714923393aaf9382abf83579aae2066ab7bdbf8d
Mike Cioffi <mike.cioffi95@gmail.com>
2022-11-21 12:48:32 -0600

    fix: enable feature buttons to go past .09

--- src/views/Feature.vue
+++ src/views/Feature.vue
@@ -53,7 +53,7 @@
                 type="number"
                 step="0.01"
                 min="0.01"
-                max=".099"
+                max=".99"
                 :value="getFeatureSettings(environment)?.seed"
                 @input="
                   handleSeedChange(environment, $event.target.value * 100)
